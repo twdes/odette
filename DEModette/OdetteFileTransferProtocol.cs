5d3345daddd98f81c5912e454bbbe6093c189ac3
neolithos <neolithos@outlook.com>
2015-10-28 14:17:28 +0100

    Initial structure

---
+++ DEModette/OdetteFileTransferProtocol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TecWare.DE.Server;
+
+namespace TecWare.DE.Odette
+{
+	///////////////////////////////////////////////////////////////////////////////
+	/// <summary></summary>
+	public class OdetteFileTransferProtocolItem : DEConfigLogItem
+	{
+		public OdetteFileTransferProtocolItem(IServiceProvider sp, string name)
+			: base(sp, name)
+		{
+		} // ctor
+	} // class OdetteFileTransferProtocolItem
+}
---
+++ DEModette/Services/DirectoryFileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TecWare.DE.Server;
+
+namespace TecWare.DE.Odette.Services
+{
+	///////////////////////////////////////////////////////////////////////////////
+	/// <summary></summary>
+	public class DirectoryFileServiceItem : DEConfigLogItem
+	{
+		public DirectoryFileServiceItem(IServiceProvider sp, string name)
+			: base(sp, name)
+		{
+		} // ctor
+	} // class DirectoryFileServiceItem
+}
