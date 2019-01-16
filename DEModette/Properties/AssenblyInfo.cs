b8c476bf37ebddb71a95a404129578644314803c
neolithos <neolithos@outlook.com>
2019-01-16 12:56:46 +0100

    Fix: AssemblyInfo

---
+++ DEModette/Properties/AssenblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using TecWare.DE.Odette;
+using TecWare.DE.Server.Configuration;
+
+[assembly: ComVisible(false)]
+
+[assembly: DEConfigurationSchema(typeof(OdetteFileTransferProtocolItem), "DEModette.xsd")]
+[assembly: Guid("72eed527-236e-4124-b360-54b25f0f9105")]
+
+[assembly: InternalsVisibleTo("ServerTests")]
